07a7fa59cbc93c858d959fae78e84c1c1418113e
Gepoverlow <gepeoverload@gmail.com>
2021-07-03 19:19:49 +0200

    made improvements today

--- javascript.js
+++ javascript.js
@@ -1,38 +1,82 @@
+"use strict";
+/*
 const container = document.querySelector(".containerMain");
 const reset = document.querySelector(".resetBtn");
+const test = document.querySelector(".test");
 
-// for (let i = 0; i <= 16; i++) {
-//   const grid = document.createElement("div");
-//   container.appendChild("grid");
-//   grid.classList.add("grid");
-// }
+let grid = document.createElement("div");
+let grids = document.querySelectorAll(".grid");
+
+for (let i = 0; i < 100; i++) {
+  grid = document.createElement("div");
+  grid.classList.add("grids");
+  container.appendChild(grid);
+}
 
 const calculatePrompt = function (number) {
   return number * number;
 };
-/*
-const stored = function () {
-  let grid = document.createElement("div");
-  grid.classList.add("grids");
-  return grid * 7;
-};
-*/
 
-// for (let i = 0; i < 4; i++) {
-//   let grid = document.createElement("div");
-//   grid.classList.add("grids");
-//   container.appendChild(grid);
-// }
+test.addEventListener("click", function () {
+  while (grid === grid) container.removeChild(grid);
+});
 
 reset.addEventListener("click", function () {
   let request = prompt("Pick a number from 1 to 10");
   let requested = calculatePrompt(request);
-
   for (let i = 0; i < requested; i++) {
-    let grid = document.createElement("div");
+    grid = document.createElement("div");
     grid.classList.add("grids");
     container.appendChild(grid);
     container.style.gridTemplateColumns = `repeat(${request}, auto)`;
     container.style.gridTemplateRows = `repeat(${request}, auto)`;
   }
+});
+
+console.log(grid);
+*/
+/////////////////////////////////////////////////
+
+document.addEventListener("DOMContentLoaded", function () {
+  const container = document.querySelectorAll(".containerMain")[0];
+  for (let i = 0; i < 36; i++) {
+    let grid = document.createElement("div");
+    container.appendChild(grid);
+    grid.classList.add("grids");
+  }
+  const grids = document.querySelectorAll(".grids");
+  for (let i = 0; i < grids.length; i++) {
+    grids[i].addEventListener("click", function () {
+      grids[i].classList.add("hover");
+    });
+  }
+  const reset = document.querySelector(".resetBtn");
+  reset.addEventListener("click", function () {
+    for (let i = 0; i < grids.length; i++) {
+      grids[i].classList.remove("hover");
+    }
+  });
+  const resize = document.querySelector(".test");
+  resize.addEventListener("click", function () {
+    let request = prompt("pick a number");
+    let requested = request * request;
+    container.innerHTML = "";
+    if (request > 0 && request <= 53) {
+      for (let i = 0; i < requested; i++) {
+        let grid = document.createElement("div");
+        container.appendChild(grid);
+        grid.classList.add("grids");
+        container.style.gridTemplateColumns = `repeat(${request}, auto)`;
+        container.style.gridTemplateRows = `repeat(${request}, auto)`;
+      }
+      const grids = document.querySelectorAll(".grids");
+      for (let i = 0; i < grids.length; i++) {
+        grids[i].addEventListener("click", function () {
+          grids[i].classList.add("hover");
+        });
+      }
+    } else {
+      prompt("The Number has to be between 1 and 54");
+    }
+  });
 });
